92ecb9218b37fa65046d6f86202beb539471805b
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-01-04 20:03:27 +0530

    Time: 514 ms (40.81%), Space: 104.6 MB (34.09%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -8,34 +8,8 @@ public:
         for(auto it:m)
         {
             if(it.second==1)
-            {
-              return -1;  
-            } 
-            else
-            {
-               cout<<it.first<<"->"<<it.second<<endl;
-                while(it.second>2)
-                {
-                    int temp1=it.second%3;
-                    int rem1=it.second/3;
-                    int rem2,temp2;
-                   // cout<<it.second<<" temp->"<<temp<<" Rem->"<<rem<<endl;
-                    if(temp1==1)
-                    {
-                        temp2=it.second%2;
-                        rem2=it.second/2;
-                        rem1=min((temp1+rem1),rem2);
-                    }
-                    
-                    it.second=temp1;
-                   // cout<<it.second<<" tempb->"<<temp<<" Remb->"<<rem<<endl;
-                    cnt+=rem1;
-                }
-               
-                if(it.second==2)
-                    cnt++;
-             // cout<<"cnt "<<cnt<<endl;
-            }
+                return -1;
+            cnt+=(it.second+2)/3;
         }
         return cnt;
     }
