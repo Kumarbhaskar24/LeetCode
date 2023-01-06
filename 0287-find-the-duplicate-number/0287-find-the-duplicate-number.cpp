beecaec34bcdf28bfe2554c08019a5a8f7565195
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-01-06 19:23:59 +0530

    Time: 483 ms (8.86%), Space: 61.3 MB (65.56%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-    {
-    // int xs=0;
-    // int ans=0;
-    // for(int i=0;i<nums.size();i++)
-    // {
-    // for(int j=i+1;j<nums.size();j++)
-    // {
-    // // cout<<nums[i]<<" "<<nums[j]<<endl;
-    // xs=nums[i]^nums[j];
-    // if(xs==0)
-    // {
-    //     ans=nums[j];
-    //     return nums[j];
-    // }
-    // }
-    // }
-    // return ans;
-    }    
-      //  cout<<nums.size();
-        vector<int> arr(nums.size(), 0);
-       
-        for(int i=0;i<nums.size();i++)
-        {  
-            arr[nums[i]]++;
-        //    cout<<nums[i]<<"->"<<arr[nums[i]]<<endl;
-             
+    
+     int n = nums.size()-1, res = 0;
+    for (int p = 0; p < 32; ++ p) {
+        int bit = (1 << p), a = 0, b = 0;
+        for (int i = 0; i <= n; ++ i) {
+            if (i > 0 && (i & bit) > 0) ++a;
+            if ((nums[i] & bit) > 0) ++b;
         }
-        for(int i=1;i<nums.size();i++)
+        if (b > a) res += bit;
+    }
+    return res;
+     
         {
-            if(arr[i]>1)
-                return i;
+//         vector<int> arr(nums.size(), 0);
+       
+//         for(int i=0;i<nums.size();i++)
+//         {  
+//             arr[nums[i]]++;
+
+             
+//         }
+//         for(int i=1;i<nums.size();i++)
+//         {
+//             if(arr[i]>1)
+//                 return i;
+//         }
+//         return 0;
         }
-        return 0;
         }
 };
