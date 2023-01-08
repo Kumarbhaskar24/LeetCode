962b031663a55da92ca6c74bf765ef541e5fd67f
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-01-09 01:00:03 +0530

    Time: 58 ms (67.50%), Space: 17.7 MB (33.89%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
-    int maxPoints(vector<vector<int>>& points) {
-    //  vector<int> temp;
-        
+    int maxPoints(vector<vector<int>>& points) {        
         float x=0;
         float y=0;
          int maxi=0;
-        if(points.size()==1)
-            return 1;
        for(int i=0;i<points.size();i++)
            {
-           map<float,int> ma;
+           unordered_map<float,int> ma;
           
            float m=0;
            x=points[i][0];
@@ -19,8 +15,6 @@ public:
            {
                float x1=points[j][0];
                float y1=points[j][1];
-               // cout<<x1<<" "<<y1<<endl;
-               // cout<<x-x1<<endl;
                if(x==x1&&y==y1)
                    continue;
               if(x-x1!=0)
@@ -33,22 +27,6 @@ public:
                }
                ma[m]++;
                maxi=max(maxi,ma[m]);
-           }
-           
-           {
-           //cout<<"above ma"<<endl;
-//            for(auto it:ma)
-//            {
-//               // cout<<it.first<<" "<<it.second<<endl;
-//                if(it.second>=maxi)
-//                {
-//                    maxi=it.second;
-//                     val=maxi+1;
-                  
-//                }
-//            }
-          // temp.push_back(val);
-           
            }
        }
       return maxi+1;
