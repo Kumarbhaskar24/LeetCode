dff4221ad08b2945ca35f839c6b579662e53bc07
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-01-22 13:24:41 +0530

    Time: 6 ms (95.31%), Space: 10.5 MB (6.69%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,21 +1,57 @@
+// class Solution {
+// public:
+//     int helper(vector<vector<int>>& grid,int m,int n,vector<vector<int>> &dp)
+//     {
+//         dp[m-1][n-1]=grid[m-1][n-1];
+//         for(int i=m-2;i>=0;i--)
+//             dp[i][n-1]=grid[i][n-1]+dp[i+1][n-1];
+       
+//         for(int i=n-2;i>=0;i--)
+//             dp[m-1][i]=grid[m-1][i]+dp[m-1][i+1];
+        
+//         for(int i=m-2;i>=0;i--)
+//         {
+//             for(int j=n-2;j>=0;j--)
+//             {
+//                 dp[i][j]=min(dp[i][j+1],dp[i+1][j])+grid[i][j];
+//             }
+//         }
+//         return dp[0][0];
+//     }
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int m=grid.size();
+//         int n=grid[0].size();
+//         vector<vector<int>> dp(m,vector<int> (n,0));
+//         return helper(grid,m-1,n-1,dp);
+//     }
+// };
 class Solution {
 public:
-    int helper(vector<vector<int>>& grid,int m,int n,vector<vector<int>> &dp)
-    {
-        if(m<0||n<0)
-            return INT_MAX;
-        else if(m==0&&n==0)
-            return grid[m][n];
-        if(dp[m][n]!=-1)
-            return dp[m][n];
-        int ans= min(helper(grid,m-1,n,dp),helper(grid,m,n-1,dp))+grid[m][n];
-        dp[m][n]=ans;
-        return ans;
-    }
+    int minCost(vector<vector<int>> &cost,int m, int n,vector<vector<int>> dp) { 
+      dp[m-1][n-1]=cost[m-1][n-1];
+      for(int i=n-2;i>=0;i--){
+          dp[m-1][i]=dp[m-1][i+1]+cost[m-1][i];
+      } 
+      for(int i=m-2;i>=0;i--){
+          dp[i][n-1]=dp[i+1][n-1]+cost[i][n-1];
+      }
+      for(int i=m-2;i>=0;i--){
+          for(int j=n-2;j>=0;j--){
+              dp[i][j]=cost[i][j]+min(dp[i+1][j],dp[i][j+1]);
+          }
+      }
+      return dp[0][0];
+    } 
+    
     int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        return helper(grid,m-1,n-1,dp);
+        int m=grid.size(),n=grid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n, 0));
+        return minCost(grid,m,n,dp);
     }
+    
 };
+
+
+
+
+
