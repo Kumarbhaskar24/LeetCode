d74325bc7305b72296c5a802bcac8043a615b17f
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-01-31 20:54:19 +0530

    Time: 7 ms (96.94%), Space: 10.4 MB (76.30%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
     int ans=1;
-    int helper(vector<int> &nums,vector<vector<int>> &dp,int i,int j)
-    {
-        if(i==nums.size())
-            return 0;
+//     int helper(vector<int> &nums,vector<vector<int>> &dp,int i,int j)
+//     {
+//         if(i==nums.size())
+//             return 0;
        
-        if(dp[i][j+1]!=-1)
-            return dp[i][j+1];
+//         if(dp[i][j+1]!=-1)
+//             return dp[i][j+1];
         
-        int take=0;
-        //include the particular number
-        if(j==-1||nums[i]>nums[j])
-            take=1+helper(nums,dp,i+1,i);
+//         int take=0;
+//         //include the particular number
+//         if(j==-1||nums[i]>nums[j])
+//             take=1+helper(nums,dp,i+1,i);
         
-        //exclude the current value 
-        int nottake=0;
-            nottake=0+helper(nums,dp,i+1,j);
-        return dp[i][j+1]=max(take,nottake);
+//         //exclude the current value 
+//         int nottake=0;
+//             nottake=0+helper(nums,dp,i+1,j);
+//         return dp[i][j+1]=max(take,nottake);
+//     }
+    
+    int helper_binary_search(vector<int> &nums)
+    {
+        if(nums.size()==0)
+            return 0;
+        vector<int> ans;
+        ans.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]>ans.back())
+                ans.push_back(nums[i]);
+            else
+            {
+                int index=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                ans[index]=nums[i];
+            }
+        }
+        return ans.size();
     }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int> (nums.size()+1,-1));
-        return helper(nums,dp,0,-1);
+        // vector<vector<int>> dp(nums.size(),vector<int> (nums.size()+1,-1));
+        // return helper(nums,dp,0,-1);
+        return helper_binary_search(nums);
     }
 };
