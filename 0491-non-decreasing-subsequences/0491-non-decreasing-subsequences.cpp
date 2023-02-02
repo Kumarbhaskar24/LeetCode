63caa2f1f8c5d84fb2fec3eabf377c39991e773c
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-02-02 17:20:57 +0530

    Time: 1174 ms (5.01%), Space: 478.5 MB (5.49%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     set<vector<int>> ans;
-    void solve(vector<int> &nums,int i,int prev,vector<int> temp)
+    int solve(vector<int> &nums,int i,int prev,vector<int> temp,vector<vector<int>> dp)
     {
         if(i==nums.size())
-            return ;
+            return 0;
+        if(dp[i][prev+1]!=-1)
+            return dp[i][prev+1];
         if(prev==-1||nums[i]>=nums[prev])
         {
             temp.push_back(nums[i]);
             if(temp.size()>1)
                 ans.insert(temp);
-            solve(nums,i+1,i,temp);
+            dp[i][prev+1]=solve(nums,i+1,i,temp,dp);
             temp.pop_back();
         }
-        solve(nums,i+1,prev,temp);
-        return ;
+        return dp[i][prev+1]=solve(nums,i+1,prev,temp,dp);
+       
     }
     vector<vector<int>> findSubsequences(vector<int>& nums) {
         vector<int> temp;
-        solve(nums,0,-1,temp);
+        vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
+        solve(nums,0,-1,temp,dp);
         return vector(ans.begin(),ans.end());
     }
 };
