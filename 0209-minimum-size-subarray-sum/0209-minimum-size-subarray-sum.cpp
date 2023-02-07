d7f16e0d47a71a93fbc218843e96130d36b4cf3d
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-02-08 01:49:19 +0530

    Time: 47 ms (43.48%), Space: 29.3 MB (5.94%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,43 +1,15 @@
 class Solution {
 public:
-    int minSubArrayLen(int target, vector<int>& nums) {
-        // int sum=0;
-        // vector<int> v;
-        // int cnt=0;
-        // int ans=INT_MAX;
-        // for(int i=0;i<nums.size();i++)
-        // {
-        //     sum+=nums[i];
-        //     if(v.back()>=target)
-        //     {
-        //         int temp=target-v.back();
-        //         int index=v.upper_bound(v.begin(),v.end())-v.begin();
-        //     }
-        //     v.push_back(sum);
-        //     cnt++;
-        // }
-        // return ans==INT_MAX?0:ans;
-        
-        int i=0;
-        int j=0;
-        int sum=0;
-        int ans=INT_MAX;
-        while(j<nums.size())
-        {
-            sum=sum+nums[j];
-            int res=0;
-            if(sum>=target)
-            {
-                while(sum>=target&&i<=j)
-                {
-                    res=j-i+1;
-                    ans=min(ans,res);
-                    sum-=nums[i];
-                    i++;  
-                }
-            }
-            j++;
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int n = nums.size(), len = INT_MAX;
+        vector<int> sums(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1];
         }
-        return ans==INT_MAX?0:ans;
+        for (int i = n; i >= 0 && sums[i] >= s; i--) {
+            int j = upper_bound(sums.begin(), sums.end(), sums[i] - s) - sums.begin();
+            len = min(len, i - j + 1);
+        }
+        return len == INT_MAX ? 0 : len;
     }
 };
