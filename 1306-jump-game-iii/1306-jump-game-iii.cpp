005d68679262f891021f607a19ab65ecdb200a83
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-02-11 01:29:20 +0530

    Time: 62 ms (28.02%), Space: 34.8 MB (49.40%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    
-    bool solve(vector<int> &nums,int start,vector<int> &dp)
-    {
-        if(start<0||start>=nums.size())
-            return false;
-        if(nums[start]==0)
-            return true;
-        if(dp[start]==-1)
-            return false;
-        dp[start]=-1;
-        bool a=solve(nums,start-nums[start],dp);
-        bool b=solve(nums,start+nums[start],dp);
-        return a||b;
-    }
-    
     bool canReach(vector<int>& arr, int start) {
-        vector<int> dp(arr.size()+1,0);
-        return solve(arr,start,dp);
+       
+        if(arr[start]== -1) 
+            return false;
+        if(!arr[start]) 
+            return true;
+           
+        int leftside= start-arr[start] ,rightside = start+arr[start];
+        arr[start] = -1;
+        return rightside = (rightside < arr.size() && canReach(arr,rightside) ) || (leftside>=0 && canReach(arr,leftside) );
     }
 };
