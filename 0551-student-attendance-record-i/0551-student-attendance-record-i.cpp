275ca1f3d54c325da521057473a0c5c38758d2c9
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-02-12 17:53:44 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (60.08%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     bool checkRecord(string s) {
-        unordered_map<char,int> m;
-        int cnt=1;
-        if(s.size()==1)
-            return true;
-        for(int i=1;i<s.size();i++)
+        int L=0;
+        int A=0;
+        for(int i=0;i<s.size();i++)
         {
-            if(s[i-1]==s[i])
-                cnt++;
-            else
+            if(s[i]=='A')
             {
-                if(cnt>=3&&s[i-1]=='L')
+                L=0;
+                A++;
+                if(A==2)
                     return false;
-                m[s[i-1]]+=cnt;
-                cnt=1;
             }
-        }
-        if(cnt>=3&&s[s.size()-1]=='L')
-        {
-            return false;
-        }
-        if(cnt!=1)
-            m[s[s.size()-1]]+=cnt;
-        for(auto it:m)
-        {
-            if(it.first=='A'&&it.second>=2)
-                return false;
+            else if(s[i]=='L')
+            {
+                L++;
+                if(L==3)
+                    return false;
+            }
+            else L=0;
         }
         return true;
     }
