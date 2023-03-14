a9d097e25fdb2512b69a4fe9f6b728ce1efa87b1
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-03-14 22:08:49 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (18.96%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -11,33 +11,8 @@
  */
 class Solution {
 public:
-   // {
-        // int height(TreeNode* root)
-    // {
-    //     if(root==NULL)
-    //         return 0;
-    //     return max(height(root->left),height(root->right))+1;
-    // }
-    // int helper(TreeNode* root,int h,int sum,int ans)
-    // {
-    //     if(root==NULL)
-    //         return 0;
-    //         h--;
-    //         sum=sum+(root->val*pow(10,h));
-    //         cout<<"Sum ="<<sum<<endl;
-    //         helper(root->left,h,sum,ans);
-    //         cout<<"Sum after left ="<<sum<<endl;
-    //     //    ans=ans+sum;
-    //        // sum=sum-(root->val*pow(10,h));
-    //         cout<<"Sum before right ="<<sum<<endl;
-    //         helper(root->right,h,sum,ans);
-    //         ans=ans+sum;
-    //      cout<<"ans after right ="<<ans<<endl;
-    //    return ans;
-    // }
-  //  }
     int sumNumbers(TreeNode* root) {
-        if(root==NULL)
+                if(root==NULL)
             return 0;
         queue<pair<TreeNode*,int>> q;
         q.push({root,0});
