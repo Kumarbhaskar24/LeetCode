c1562a23a238277bf24e23ad29ff41da6ee1d926
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-03-27 01:00:32 +0530

    Time: 7 ms (52.23%), Space: 16.4 MB (84.23%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -13,27 +13,12 @@ class Solution {
 public:
     bool symmetric(TreeNode* root1,TreeNode* root2)
     { 
-        
         if(root1==NULL&&root2==NULL)
-        {
-       
             return true;
-            //cout<<root1->val<<" inside and"<<root2->val<<endl;
-        }
          if(root1==NULL||root2==NULL)
-        {
-       
             return false;
-            //cout<<root1->val<<" inside and"<<root2->val<<endl;
-        }
         if(root1->val!=root2->val)
-        {
-         cout<<root1->val<<"and"<<root2->val<<endl;
             return false;
-        }
-        cout<<root1->val<<" after and"<<root2->val<<endl;
-            // else if(root1->value==root2->value)
-        //     return true;
         return symmetric(root1->left,root2->right)&&symmetric(root1->right,root2->left);
     }
     
