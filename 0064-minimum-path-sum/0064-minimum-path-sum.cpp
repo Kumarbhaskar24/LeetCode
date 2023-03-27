03aa8e79808e45f2a2090dbd0fdb693260628d47
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-03-28 01:10:21 +0530

    Time: 1200 ms (5.35%), Space: 10.1 MB (36.09%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,57 +1,25 @@
-// class Solution {
-// public:
-//     int helper(vector<vector<int>>& grid,int m,int n,vector<vector<int>> &dp)
-//     {
-//         dp[m-1][n-1]=grid[m-1][n-1];
-//         for(int i=m-2;i>=0;i--)
-//             dp[i][n-1]=grid[i][n-1]+dp[i+1][n-1];
-       
-//         for(int i=n-2;i>=0;i--)
-//             dp[m-1][i]=grid[m-1][i]+dp[m-1][i+1];
-        
-//         for(int i=m-2;i>=0;i--)
-//         {
-//             for(int j=n-2;j>=0;j--)
-//             {
-//                 dp[i][j]=min(dp[i][j+1],dp[i+1][j])+grid[i][j];
-//             }
-//         }
-//         return dp[0][0];
-//     }
-//     int minPathSum(vector<vector<int>>& grid) {
-//         int m=grid.size();
-//         int n=grid[0].size();
-//         vector<vector<int>> dp(m,vector<int> (n,0));
-//         return helper(grid,m-1,n-1,dp);
-//     }
-// };
 class Solution {
 public:
-    int minCost(vector<vector<int>> &cost,int m, int n,vector<vector<int>> dp) { 
-      dp[m-1][n-1]=cost[m-1][n-1];
-      for(int i=n-2;i>=0;i--){
-          dp[m-1][i]=dp[m-1][i+1]+cost[m-1][i];
-      } 
-      for(int i=m-2;i>=0;i--){
-          dp[i][n-1]=dp[i+1][n-1]+cost[i][n-1];
-      }
-      for(int i=m-2;i>=0;i--){
-          for(int j=n-2;j>=0;j--){
-              dp[i][j]=cost[i][j]+min(dp[i+1][j],dp[i][j+1]);
-          }
-      }
-      return dp[0][0];
-    } 
     
-    int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size(),n=grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n, 0));
-        return minCost(grid,m,n,dp);
+    int mini=INT_MAX;
+    int solve(vector<vector<int>> &nums,int i,int j,int sum,vector<vector<int>> &dp)
+    {
+        if(i>=nums.size()||j>=nums[0].size())
+            return 0;
+        sum+=nums[i][j];
+        if(dp[i][j]!=-1&&dp[i][j]<=sum)
+            return dp[i][j];
+        dp[i][j]=sum;
+        solve(nums,i+1,j,sum,dp);
+        solve(nums,i,j+1,sum,dp);
+        if(i==nums.size()-1&&j==nums[0].size()-1)
+            mini=min(mini,sum);
+        return mini;
     }
     
+    
+    int minPathSum(vector<vector<int>>& grid) {
+        vector<vector<int>> dp(grid.size(),vector<int> (grid[0].size(),-1));
+        return solve(grid,0,0,0,dp);
+    }
 };
-
-
-
-
-
