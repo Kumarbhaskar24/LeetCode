284ca77365196d9bdad6f05e34f2147610405b03
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-04-04 22:00:38 +0530

    Time: 296 ms (5.94%), Space: 72 MB (21.72%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        int ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            bool a=true;
+            map<char,int> m;
+            while(a&&i<s.size())
+            {
+                if(m[s[i]]==1)
+                    a= false;
+                else{
+                    m[s[i]]++;
+                    i++;
+            }
+            }
+            i--;
+            ans++;
+        }
+        return ans;
+    }
+};
