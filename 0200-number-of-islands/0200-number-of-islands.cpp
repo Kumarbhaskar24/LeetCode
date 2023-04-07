b9a3128078c8cb607467214d064c2f8d8cf4677d
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-04-07 23:09:14 +0530

    Time: 32 ms (91.19%), Space: 12.2 MB (87.80%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,47 +1,50 @@
 class Solution {
 public:
     
-    void solve(int i,int j,vector<vector<char>> &grid,vector<vector<int>> &vis)
+    bool isValid(int i,int j,int n,int m,vector<vector<char>> &grid)
     {
-        int m=grid.size();
-        int n=grid[0].size();
-        queue<pair<int,int>> q;
-        q.push({i,j});            
-        while(!q.empty())
+        if(i>=0&&i<n&&j>=0&&j<m&&grid[i][j]=='1')
+            return true;
+        return false;
+    }
+    
+    void dfs(int i,int j,int n,int m,vector<vector<char>> &grid)
+    {
+        grid[i][j]='B';
+        int ax[4]={1,-1,0,0};
+        int ay[4]={0,0,1,-1};
+        for(int k=0;k<4;k++)
         {
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
-            vector<vector<int>> coord={{-1,0},{0,-1},{1,0},{0,1}};
-            for(int k=0;k<4;k++)        
+            int nx=i+ax[k];
+            int ny=j+ay[k];
+            if(isValid(nx,ny,n,m,grid))
             {
-                int nrow=r+coord[k][0];
-                int ncol=c+coord[k][1];
-                if(nrow>=0&&nrow<m&&ncol<n&&ncol>=0&&grid[nrow][ncol]=='1'&&!vis[nrow][ncol])
-                {
-                    q.push({nrow,ncol});
-                    vis[nrow][ncol]=1;
-                }
+                dfs(nx,ny,n,m,grid);
             }
         }
     }
     
     int numIslands(vector<vector<char>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        int cnt=0;
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(!vis[i][j]&&grid[i][j]=='1')
+        int n=grid.size();
+        int m=grid[0].size();
+        int ans=0;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if(i*j==0||i==n-1||j==m-1)
+                    if(grid[i][j]=='1')
+                    { 
+                        ans++;
+                        dfs(i,j,n,m,grid);
+                    }
+
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+              if(grid[i][j]=='1')
                 {
-                    cnt++;
-                    solve(i,j,grid,vis);
+                    ans++;
+                    dfs(i,j,n,m,grid);
                 }
-            }
-        }
-        return cnt;
+ 
+        return ans;
     }
 };
