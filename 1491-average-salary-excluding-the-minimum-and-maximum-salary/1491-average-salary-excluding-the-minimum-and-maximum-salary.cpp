3d349b66323963c7bd32a24a0936c603d98f5716
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-05-01 10:13:56 +0530

    Time: 5 ms (23.02%), Space: 7 MB (98.74%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -4,11 +4,7 @@ public:
         sort(salary.begin(),salary.end());
         double ans=0;
         for(int i=1;i<salary.size()-1;i++)
-        {
             ans+=salary[i];
-        //    cout<<salary[i]<<" ";
-        }
-      //  cout<<ans<<" "<<salary.size()-2;
         return ans/(salary.size()-2);
     }
 };
