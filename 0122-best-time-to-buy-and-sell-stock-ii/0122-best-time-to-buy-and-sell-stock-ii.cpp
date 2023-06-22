756866f3a1c88a947b8faa64e11e1541a5dea40b
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-06-22 15:03:57 +0530

    Time: 10 ms (46.93%), Space: 13 MB (47.35%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-	int f(int i,int buy,vector<int>& p,int n,vector<vector<int>>& dp){
-		if(i==n) return 0;
-		if(dp[i][buy]!=-1) return dp[i][buy];
-		int pro1=INT_MIN,pro2=INT_MIN;
-		if(buy){
-			int buy=-p[i]+f(i+1,0,p,n,dp);
-			int notbuy=f(i+1,1,p,n,dp);
-			pro1=max(buy,notbuy);
-		}
-		else{
-			int sell= p[i]+f(i+1,1,p,n,dp);
-			int notsell=f(i+1,0,p,n,dp);
-			pro2=max(sell,notsell);
-		}
-		return dp[i][buy]=max(pro1,pro2);
-	}
-
-	int maxProfit(vector<int>& p){
-		int n=p.size();
-		vector<vector<int>> dp(n,vector<int>(2,-1));
-		return f(0,1,p,n,dp);
-	}
+    int maxProfit(vector<int>& prices) {
+        int mini=prices[0];
+        int profit =0;
+        for(int i=1;i<prices.size();i++)
+        {
+            if(prices[i]<mini)
+                mini=prices[i];
+            else{
+                profit+=prices[i]-mini;
+                mini=prices[i];
+            }
+        }
+        return profit;
+    }
 };
