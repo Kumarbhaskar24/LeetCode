e1ff6250e138b8bf17fbc7eb7df3cd888ca4c65e
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-06-23 14:43:49 +0530

    Time: 1804 ms (5.00%), Space: 292.3 MB (7.61%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,46 +1,25 @@
 class Solution {
 public:
-    int ans=1;
-//     int helper(vector<int> &nums,vector<vector<int>> &dp,int i,int j)
-//     {
-//         if(i==nums.size())
-//             return 0;
-       
-//         if(dp[i][j+1]!=-1)
-//             return dp[i][j+1];
-        
-//         int take=0;
-//         //include the particular number
-//         if(j==-1||nums[i]>nums[j])
-//             take=1+helper(nums,dp,i+1,i);
-        
-//         //exclude the current value 
-//         int nottake=0;
-//             nottake=0+helper(nums,dp,i+1,j);
-//         return dp[i][j+1]=max(take,nottake);
-//     }
     
-    int helper_binary_search(vector<int> &nums)
+    int solve(int i,int prev,vector<int> &nums,vector<vector<int>> &dp)
     {
-        if(nums.size()==0)
+        if(i==nums.size())
             return 0;
-        vector<int> ans;
-        ans.push_back(nums[0]);
-        for(int i=1;i<nums.size();i++)
+        if(dp[i][prev+1]!=-1)
+            return dp[i][prev+1];
+        int take=0;
+        if(prev==-1||nums[i]>nums[prev])
         {
-            if(nums[i]>ans.back())
-                ans.push_back(nums[i]);
-            else
-            {
-                int index=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
-                ans[index]=nums[i];
-            }
+            take=1+solve(i+1,i,nums,dp);
         }
-        return ans.size();
+        int nottake=0;
+            nottake=solve(i+1,prev,nums,dp);
+        return dp[i][prev+1]= max(take,nottake);
     }
+    
+    
     int lengthOfLIS(vector<int>& nums) {
-        // vector<vector<int>> dp(nums.size(),vector<int> (nums.size()+1,-1));
-        // return helper(nums,dp,0,-1);
-        return helper_binary_search(nums);
+        vector<vector<int>> dp(nums.size()+1,vector<int> (nums.size()+1,-1));
+        return solve(0,-1,nums,dp);      
     }
 };
