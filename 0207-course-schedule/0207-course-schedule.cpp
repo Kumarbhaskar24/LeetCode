7a2fad68ac84f9c28626a2fc64858dcfb332ab14
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-06-27 01:09:16 +0530

    Time: 27 ms (44.41%), Space: 13.5 MB (74.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,41 +1,33 @@
 class Solution {
 public:
-    
-    bool iscycle(int i,vector<vector<int>>& nums,vector<bool> &vis,vector<bool> &dfsvis,vector<vector<int>> &adj)
-    {
-        vis[i]=true;
-        dfsvis[i]=true;
-        vector<int> data=adj[i];
-        for(auto it:data)
-        {
-            if(!vis[it])
-            {
-                if(iscycle(it,nums,vis,dfsvis,adj))
-                    return true;
-            }
-            else if(dfsvis[it]&&vis[it])
-                return true;           
-        }
-        dfsvis[i]=false;
-        return false;
-        
-    }
-    
-    
-    bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        vector<bool> vis(n,false);
-        vector<bool> dfsvis(n,false);
-        vector<vector<int>> adj(n);
-        for(auto it:prerequisites)
-        {
-            adj[it[0]].push_back(it[1]);
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
-                if(iscycle(i,prerequisites,vis,dfsvis,adj))
-                    return false;
-        }
-        return true;
+    bool canFinish(int n, vector<vector<int>>& pre) {
+     vector<vector<int>>adj(n);
+     vector<int>indeg(n,0);
+     for(int i=0;i<pre.size();i++){
+         adj[pre[i][1]].push_back(pre[i][0]);
+         indeg[pre[i][0]]++;
+     }
+    // for(int i=0;i<indeg.size();i++)
+    //     cout<<i<<" "<<indeg[i]<<endl;
+     queue<int>q;
+     for(int i=0;i<n;i++){
+         if(indeg[i] == 0){
+             q.push(i);
+         }
+     }
+     vector<int>topo;
+     while(!q.empty()){
+         int node= q.front();
+         q.pop();
+         topo.push_back(node);
+         for(int x:adj[node]){
+             indeg[x]--;
+             if(indeg[x] == 0){
+                 q.push(x);
+             }
+         }
+     }
+     //if topo size is == n then means it does not have a cycle so we can schedule the courses
+     return topo.size() == n;
     }
 };
