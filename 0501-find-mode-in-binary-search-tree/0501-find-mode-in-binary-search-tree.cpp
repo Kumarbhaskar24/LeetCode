e11143a9bf67ccfbdaaa31379f446628f784de5a
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-11-01 19:00:46 +0530

    Time: 12 ms (84.62%), Space: 24.4 MB (78.98%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -11,37 +11,32 @@
  */
 class Solution {
 public:
-    void inorder(unordered_map<int,int> &m,TreeNode* root)
+    int maxFreq = 0, currFreq = 0, precursor = INT_MIN;
+    vector<int> res;
+
+    vector<int> findMode(TreeNode *root)
     {
-        if(root==NULL)
-            return;
-        inorder(m,root->left);
-        m[root->val]++;
-        inorder(m,root->right);
-        return;
-        
+        inorderTraversal(root);
+        return res;
     }
-    vector<int> findMode(TreeNode* root) {
-        vector<int> ans;
-        if(root==NULL)
-            return ans;
-        unordered_map<int,int> m;
-        inorder(m,root);
-        int temp=0;
-        int first;
-        for(auto it:m)
-        {
-            if(it.second>=temp)
-            {
-                first=it.first;
-                temp=it.second;
-            }
+
+    void inorderTraversal(TreeNode *root)
+    {
+        if (root == NULL) return; // Stop condition
+        inorderTraversal(root->left); // Traverse left subtree
+        if (precursor == root->val) currFreq++;
+        else currFreq = 1;
+        if (currFreq > maxFreq)
+        {// Current node value has higher frequency than any previous visited
+            res.clear();
+            maxFreq = currFreq;
+            res.push_back(root->val);
         }
-        for(auto it:m)
-        {
-            if(it.second==temp)
-                ans.push_back(it.first);
+        else if (currFreq == maxFreq)
+        {// Current node value has a frequency equal to the highest of previous visited
+            res.push_back(root->val);
         }
-        return ans;
+        precursor = root->val; // Update the precursor
+        inorderTraversal(root->right); // Traverse right subtree
     }
 };
