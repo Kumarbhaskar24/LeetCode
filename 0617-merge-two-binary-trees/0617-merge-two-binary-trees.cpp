4c16de18eced6de227c6af079eec1ea2565e4950
Kumar Bhaskar <75621319+Kumarbhaskar24@users.noreply.github.com>
2023-11-29 09:32:43 +0530

    Time: 29 ms (44.04%), Space: 32.5 MB (94.32%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -11,28 +11,13 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* t1, TreeNode* t2,TreeNode* &root){
-        if(!t1 && !t2) return;
-        else if(t1 && !t2){
-            TreeNode* node(new TreeNode(t1->val));
-            root=node;
-            dfs(t1->left,t2,root->left);
-            dfs(t1->right,t2,root->right);
-        }else if(t2 && !t1){
-            TreeNode* node(new TreeNode(t2->val));
-            root=node;
-            dfs(t1,t2->left,root->left);
-            dfs(t1,t2->right,root->right);
-        }else{
-            TreeNode* node(new TreeNode(t1->val+t2->val));
-            root=node;
-            dfs(t1->left,t2->left,root->left);
-            dfs(t1->right,t2->right,root->right);
-        }
-    }
-    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        TreeNode* root(nullptr);
-        dfs(t1,t2,root);
-        return root;
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+       if(!root1||!root2)
+           return root1?root1:root2;
+        root1->val+=root2->val;
+        root1->left=mergeTrees(root1->left,root2->left);
+        root1->right=mergeTrees(root1->right,root2->right);
+        return root1;
+        
     }
 };
